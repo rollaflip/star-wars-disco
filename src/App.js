5909ca5c14620e9101dd445f3697b90afb6ce19a
Yen <iknepper@hotmail.com>
2019-01-04 17:18:58 -0500

    favicon changed

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
+          <img src="../public/clone.png" alt=''/>
             Star Wars Disco!!
           </a>
         </header>
