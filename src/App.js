c66d734dc3f580c2c364adf38327abf8c3051915
Yen <iknepper@hotmail.com>
2019-01-04 16:57:06 -0500

    testing git connection

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Star Wars Disco!!
           </a>
         </header>
       </div>
