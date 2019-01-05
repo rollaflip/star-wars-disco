541b0ae8c02cde00fa5f4993da192b493b997964
Yen <iknepper@hotmail.com>
2019-01-05 00:46:16 -0500

    CharacterList styled + imgs

--- src/App.js
+++ src/App.js
@@ -31,3 +31,4 @@ class App extends Component {
 }
 
 export default App;
+
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,23 +1,76 @@
 import React, { Component } from 'react';
-import getCharacterByName from './starWarsApiFetch'
-// import {characters} from '../characters.json'
-const characters = require('../characters')
-// import
+import getCharacterByName from './starWarsApiFetch';
+import { characters } from '../characters';
+import { Grid } from '@material-ui/core';
+import '../CharacterList.css';
 
-export default class CharacterList extends Component {
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import IconButton from '@material-ui/core/IconButton';
+import { green } from '@material-ui/core/colors';
+// import InfoIcon from '@material-ui/icons/Info';
+// import characters from './characters';
 
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    width: 500,
+    height: 450,
+    // background: 'grey',
+  },
+  icon: {
+    color: 'rgba(255, 255, 255, 0.54)',
+  },
+});
 
-  render() {
-    const charactersAll = getCharacterByName()
-    // console.log(characters)
-    return (
-      <div>
+function CharacterList(props) {
+  const { classes } = props;
 
-         {characters['characters'].map(char => {
-           return <li>{char.name}</li>
-         })}
-         {/* {console.log(characters['characters'])} */}
-      </div>
-    )
-  }
+  return (
+    <div className='wrap'>
+
+    <div className={classes.root}>
+      <GridList cellHeight={180} className={classes.gridList}>
+        <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
+          {/* <ListSubheader component="div">December</ListSubheader> */}
+        </GridListTile>
+        {characters.map(char => (
+          <GridListTile key={char.img}>
+            <img className='avatar' src={`./${char.name.split(' ')[0]}.jpeg`} alt={char.title} />
+
+            <GridListTileBar
+            style={{height: 'auto'}}
+            title={char.title}
+            subtitle={<span>{char.name}</span>}
+            actionIcon={
+              <IconButton className={classes.icon}>
+                  {/* <InfoIcon /> */}
+                </IconButton>
+              }
+              />
+          </GridListTile>
+        ))}
+      </GridList>
+    </div>
+        </div>
+  );
 }
+
+CharacterList.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(CharacterList);
+// export default class CharacterList extends Component {
+
+
