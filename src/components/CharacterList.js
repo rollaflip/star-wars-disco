f339b363fd8940bd586b70aeae2dcf07434380b4
Yen <iknepper@hotmail.com>
2019-01-04 22:48:51 -0500

    characters pulled from json and displayed in list

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import getCharacterByName from './starWarsApiFetch'
+// import {characters} from '../characters.json'
+const characters = require('../characters')
+// import
 
 export default class CharacterList extends Component {
 
 
-
-
   render() {
-    const characters = getCharacterByName()
+    const charactersAll = getCharacterByName()
+    // console.log(characters)
     return (
       <div>
-        {characters}
+
+         {characters['characters'].map(char => {
+           return <li>{char.name}</li>
+         })}
+         {/* {console.log(characters['characters'])} */}
       </div>
     )
   }
--- src/components/starWarsApiFetch.js
+++ src/components/starWarsApiFetch.js
@@ -1,15 +1,15 @@
 let axios = require('axios');
-const starWarsURL = 'https://swapi.co/api/people';
 
 const getCharacterByName = characterNum => {
+  const starWarsURL = 'https://swapi.co/api/people';
   // let encodedCharacter = encodeURIComponent(characterNum);
   let characterURL = `${starWarsURL}/${characterNum}`
   if(!characterNum) characterURL = starWarsURL
 
   return axios.get(characterURL).then(res => {
     if (res.data) {
-      let characters = res.data.results;
-      return characters;
+      let charactersFromWeb = res.data.results;
+      return charactersFromWeb;
     } else {
       throw new Error(res.data.message);
     }
