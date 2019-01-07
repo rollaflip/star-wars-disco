c3077eac9546bc33e5ee86cea61c19a06f6e48a2
Yen <iknepper@hotmail.com>
2019-01-06 19:58:14 -0500

    MovieList not receiving getMovies()

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import getMovies from './starWarsApiFetch';
-
+import axios from 'axios';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({});
@@ -14,19 +14,27 @@ class MovieList extends Component {
     console.log(props, 'props from movielist');
 
   }
-  componentDidMount(){
-    // getMovies(this.state.charDiscog)
+  // componentDidMount(){
+  //   // getMovies(this.state.charDiscog)
 
+  // }
+  async componentWillReceiveProps(newProps) {
+    // console.log(newProps.chosen, 'xxxxx')
+    if (this.props.chosen !== newProps.chosen) {
+      const discog = await getMovies(newProps.chosen)
+      // console.log(newProps)
+      this.setState({charDiscog :  discog},console.log(this.state.charDiscog))
+    }
   }
 
     render(){
       const chosen  = this.props.chosen;
       // console.log(this.props.chosen)
-      getMovies(chosen)
+      // getMovies(chosen)
       // console.log(bob)
       return (
         <div>
-        <li>{this.state.charDiscog[0]}</li>
+        {/* <li>{this.state.charDiscog[0]}</li> */}
         <p>hi</p>
       </div>
     );
--- src/components/starWarsApiFetch.js
+++ src/components/starWarsApiFetch.js
@@ -1,53 +1,41 @@
 import axios from 'axios';
-// let axios = require('axios');
-// import { characters } from '../characters';
 
-const getMovies = characterURL => {
+const getMovies = async characterURL => {
   console.log(characterURL);
-
-  // const starWarsURL = 'https://swapi.co/api/people';
-  // let encodedCharacter = encodeURIComponent(characterNum);
-  // let characterURL = `${starWarsURL}/${characterNum}`
-  // if(!characterNum) characterURL = starWarsURL
-
-  // const moviesLinks = await axios.get(characterURL).then(response => response.data.films)
-  // const dataObjs = await Promise.all(moviesLinks.map(link => axios.get(link)))
-  // console.log(dataObjs)
-  // const movies = dataObjs.map(data => data.data)
 
   const getMovieURLs = async () => {
     try {
       const charData = await axios.get(characterURL);
       return charData.data.films;
     } catch (error) {
-      return error
+      return error;
     }
   };
-  const getMovieData = async (filmURL) => {
+  const getMovieData = async filmURL => {
     try {
-      const movieData = await axios.get(filmURL)
-      return movieData
+      const movieData = await axios.get(filmURL);
+      return movieData;
     } catch (error) {
       return error;
     }
   };
-  const getDiscogLinks = async () => {
-    try{
 
-      const movieURLList = await getMovieURLs();
-      if (movieURLList) {
-        const movieDataList = await Promise.all(movieURLList.map(movieURL => {
+  try {
+    const movieURLList = await getMovieURLs();
+    if (movieURLList) {
+      const movieDataList = await Promise.all(
+        movieURLList.map(movieURL => {
           return getMovieData(movieURL);
-        }));
-        if(movieDataList){
-
-          console.log(movieDataList[3].data)
-          return movieDataList
-        }
+        })
+      );
+      if (movieDataList) {
+        console.log(movieDataList);
+        return movieDataList;
       }
-    } catch (error) {return error}
-    // else console.log('ERRRROR DOGGG');
-  };
-  getDiscogLinks();
+    }
+  } catch (error) {
+    return error;
+  }
+  // else console.log('ERRRROR DOGGG');
 };
 export default getMovies;
